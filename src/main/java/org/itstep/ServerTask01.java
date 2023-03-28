a59903977c994c7b1379635cf05dc3916ed58d28
AlexSemenyuk <Semen_jf87@student.itstep.org>
2023-03-28 15:40:37 +0300

    ready for check

--- src/main/java/org/itstep/ServerTask01.java
+++ src/main/java/org/itstep/ServerTask01.java
@@ -6,9 +6,5 @@ public class ServerTask01 {
     public static void main(String[] args) throws IOException {
         ServerHTTP serverHTTP = new ServerHTTP(80);
         serverHTTP.serverRequest();
-        while (true) {
-
-
-        }
     }
 }
